2e786e61b92a17f9003178bcacbbda2af0b9222a
delmashajan <delmashajan@gmail.com>
2022-11-30 10:59:20 +0530

    navigating

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
@@ -8,7 +9,9 @@ import { Component } from '@angular/core';
 export class AdminLoginComponent {
 
   username=""
-  password=""
+  password="" 
+
+  constructor(private route:Router){}
 
   readValue=()=>
   {
@@ -19,7 +22,7 @@ export class AdminLoginComponent {
 
     if (this.username=="admin" && this.password=="12345") {
 
-      alert("valid login")
+      this.route.navigate(['/entry'])
       
     } else {
 
